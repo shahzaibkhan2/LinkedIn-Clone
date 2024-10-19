df57e13042a7998a0cdc9645280d6b6404c3a175
shahzaibkhan2 <drshahzeb47@gmail.com>
2024-10-19 08:55:41 +0500

    Navbar development

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-const App = () => {
+const Home = () => {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="grid grid-cols-12 w-ful mx-auto px-[5%] lg:px-[10%] mt-6 gap-6">
+      <section className="hidden sm:block sm:col-span-4 lg-1:col-span-3 rounded-lg">
+        <p>Hello</p>
+      </section>
+      <section className="col-span-12 sm:col-span-8 lg-1:col-span-6 rounded-lg w-full">
+        <h1>Hello</h1>
+      </section>
+      <section className="col-span-3 rounded-lg min-h-screen hidden lg-1:block">
+        <h2>Wow</h2>
+      </section>
     </main>
   );
 };
 
-export default App;
+export default Home;
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -11,6 +11,40 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "cus-bg-dark-white": "#f4f2ee",
+      },
+      clipPath: {
+        "custom-diagonal": "polygon(0 0, 100% 0, 100% 100%, 0 60%)",
+        "bottom-diagonal": "polygon(0 0, 100% 50%, 100% 100%, 0 100%)",
+      },
+      screens: {
+        "md-xl": "900px",
+        "extra-sm": "470px",
+        "very-sm": "390px",
+        "lg-1": "900px",
+        "lg-2": "1160px",
+        "lg-3": "1270px",
+      },
+      fontSize: {
+        small: "12px",
+        medium: "14px",
+      },
+      textColor: {
+        para: "#00000099",
+        "icon-black": "#000000bf",
+        "e6-color": "#000000e6",
+      },
+      keyframes: {
+        squeezeUp: {
+          "0%": { opacity: "0", transform: "scale(0.5)" },
+          "100%": { opacity: "1", transform: "scale(1)" },
+        },
+      },
+      animation: {
+        squeezeUp: "squeezeUp 5s linear",
+      },
+      textUnderlineOffset: {
+        7: "7px",
       },
     },
   },
