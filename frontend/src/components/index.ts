ec92a95c987db229474762a6a706f3aa99d78b69
shahzaibkhan2 <drshahzeb47@gmail.com>
2024-10-19 11:15:30 +0500

    Right Feed Area Development

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,6 +2,7 @@ export { default as Navbar } from "@/components/navbar/Navbar";
 export { default as LeftNav } from "@/components/navbar/LeftNav";
 export { default as MiddleNav } from "@/components/navbar/MiddleNav";
 export { default as RightNav } from "@/components/navbar/RightNav";
+// Post Exports
 export { default as Post } from "@/components/post-feed/Post";
 export { default as PostActionActivity } from "@/components/post-feed/PostActionActivity";
 export { default as PostActivity } from "@/components/post-feed/PostActivity";
@@ -9,3 +10,19 @@ export { default as PostDesc } from "@/components/post-feed/PostDesc";
 export { default as PostMedia } from "@/components/post-feed/PostMedia";
 export { default as PostPerk } from "@/components/post-feed/PostPerk";
 export { default as PostTitle } from "@/components/post-feed/PostTitle";
+// Profile Exports
+export { default as LeftProfile } from "@/components/profiles/LeftProfile";
+export { default as ProfileViews } from "@/components/profiles/ProfileViews";
+// Recent Exports
+export { default as Recent } from "@/components/recents/Recent";
+// Right Feed Exports
+export { default as AddFeed } from "@/components/right-feed/AddFeed";
+export { default as Chat } from "@/components/right-feed/Chat";
+export { default as ChatHeader } from "@/components/right-feed/ChatHeader";
+export { default as ChatInput } from "@/components/right-feed/ChatInput";
+export { default as ChatMembers } from "@/components/right-feed/ChatMembers";
+export { default as Company } from "@/components/right-feed/Company";
+export { default as Footer } from "@/components/right-feed/Footer";
+export { default as MembersTypes } from "@/components/right-feed/MembersTypes";
+// Status Exports
+export { default as Status } from "@/components/status/Status";
