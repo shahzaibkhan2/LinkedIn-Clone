2a79fc7f53b9c21e1653942881c91be10adb8663
shahzaibkhan2 <drshahzeb47@gmail.com>
2024-10-20 17:06:29 +0500

    Profile Development

---
+++ frontend/src/components/navbar/UserProfilePopup.tsx
@@ -0,0 +1,90 @@
+import Link from "next/link";
+
+const UserProfilePopup = ({ isPoppedup }: { isPoppedup: boolean }) => {
+  return (
+    <nav
+      className={`${
+        isPoppedup ? "block" : "hidden"
+      } absolute -right-20 transition duration-300 w-72 mt-2 bg-white text-gray-800 border border-gray-300 rounded-lg shadow-lg py-2 z-10 p-2`}
+    >
+      <div className="flex gap-4 mb-3">
+        <div className="size-14">
+          <img
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
+            alt="tania andrew"
+            className="relative inline-block object-cover object-center size-full rounded-full"
+          />
+        </div>
+        <div className="flex flex-col w-52">
+          <h6 className="mb-2 text-lg antialiased text-blue-gray-900 font-semibold">
+            Shahzaib Khan
+          </h6>
+          <p className="text-md antialiased text-gray-700">
+            Full Stack MERN Developer | Frontend in React & Next.js | Redux
+            Toolkit for State Management | JavaScript & TypeScript
+          </p>
+        </div>
+      </div>
+      <Link
+        href="/profile"
+        className="flex items-center justify-center w-full px-2 bg-transparent border border-blue-700 text-blue-700 hover:ring-1 ring-blue-800 transition duration-200 rounded-full"
+      >
+        View Profile
+      </Link>
+      <hr className="my-3" />
+      <section>
+        <h6 className="font-semibold text-md my-2">Account</h6>
+        <div className="flex items-center gap-1">
+          <div className="hidden sm:block rounded-sm size-3 bg-[#F8C77E]"></div>
+          <p className="hover:text-blue-600 transition duration-200 cursor-pointer">
+            Try 1 month of Premium for PKR0
+          </p>
+        </div>
+        <div className="flex flex-col gap-3 mt-3">
+          <Link
+            href="#"
+            className="hover:underline text-gray-600 transition duration-300"
+          >
+            Settings and Privacy
+          </Link>
+          <Link
+            href="#"
+            className="hover:underline text-gray-600 transition duration-300"
+          >
+            Help
+          </Link>
+          <Link
+            href="#"
+            className="hover:underline text-gray-600 transition duration-300"
+          >
+            Language
+          </Link>
+        </div>
+      </section>
+      <hr className="my-3" />
+      <section>
+        <h6 className="font-semibold text-md my-2">Manage</h6>
+        <div className="flex flex-col gap-3">
+          <Link
+            href="#"
+            className="hover:underline text-gray-600 transition duration-300"
+          >
+            Posts & Activity
+          </Link>
+          <Link
+            href="#"
+            className="hover:underline text-gray-600 transition duration-300"
+          >
+            Job Posting Account
+          </Link>
+        </div>
+      </section>
+      <hr className="my-3" />
+      <button className="hover:underline text-gray-600 transition duration-300">
+        Sign out
+      </button>
+    </nav>
+  );
+};
+
+export default UserProfilePopup;
