a1b9be496a3510a047f81cc75bef2c69fe38d349
shahzaibkhan2 <drshahzeb47@gmail.com>
2024-10-30 11:25:36 +0500

    profile page development

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,6 +1,7 @@
 import ProfilePicture from "../components/profile/ProfilePicture";
 import ProfileTitles from "../components/profile/ProfileTitles";
 import ProfileWork from "../components/profile/ProfileWork";
+import { ImPencil } from "react-icons/im";
 
 const Profile = () => {
   return (
@@ -11,16 +12,31 @@ const Profile = () => {
         <ProfileWork />
       </section>
 
-      <section className="col-span-3 rounded-lg min-h-screen bg-blue-200">
-        <div className="p-3 space-y-3">
-          <div>
-            <h2>Profile</h2>
-            <p>Text</p>
-          </div>
-          <hr />
-          <div>
-            <h2>Profile</h2>
-            <p>Text</p>
+      <section className="col-span-3 rounded-lg min-h-screen">
+        <div className="bg-white border border-gray-300 rounded-lg">
+          <div className="p-3 flex flex-col gap-3">
+            <div className="flex flex-col gap-2">
+              <div className="flex justify-between items-center">
+                <h2 className="font-semibold text-lg text-gray-600">
+                  Profile language
+                </h2>
+
+                <ImPencil className="size-4 transition duration-300" />
+              </div>
+              <p className="text-gray-500">English</p>
+            </div>
+            <hr />
+            <div className="flex flex-col gap-2">
+              <div className="flex justify-between items-center">
+                <h2 className="font-semibold text-lg text-gray-600">
+                  Public profile & URL
+                </h2>
+                <ImPencil className="size-4 transition duration-300" />
+              </div>
+              <p className="text-gray-500">
+                www.linkedin.com/in/engr-shahzaib-khan1
+              </p>
+            </div>
           </div>
         </div>
       </section>
